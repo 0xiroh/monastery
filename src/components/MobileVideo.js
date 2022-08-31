e5f17dbc2a5330eab97e495e633f0d45714a1c54
mgutierrezo <martin72.gutierrez@gmail.com>
2022-08-31 15:42:57 -0500

    Almost ready6

--- src/components/MobileVideo.js
+++ src/components/MobileVideo.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactPlayer from 'react-player'
-import { Trans } from 'react-i18next'
+import { useTranslation, Trans } from 'react-i18next'
 
 function MobileVideo() {
+  const {t} = useTranslation();
   return (
     <div className=' md:hidden'>
     <div className='h-20'/>
-    <h1 className='text-white text-[3rem] font-extrabold'><Trans i18nKey={"title2"}/></h1>
+    <h1 className='text-white text-[2rem] font-extrabold'>{t('splitTitle1')}<br/>{t('splitTitle2')}</h1>
     <div className='flex justify-center'>
         <ReactPlayer className='' width={800} height={450} style={{ margin: '' }}  light='' controls url='https://monastery-nft.s3.amazonaws.com/resources/oculus.mp4'/>
 
