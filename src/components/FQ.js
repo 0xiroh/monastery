760dd016fa52b65a57c954158e9c440332f9cf23
mgutierrezo <martin72.gutierrez@gmail.com>
2023-02-02 10:45:22 -0500

    roadmap desktop update-2

--- src/components/FQ.js
+++ src/components/FQ.js
@@ -8,7 +8,7 @@ function FQ() {
   return (
     <div className='mt-60 sm:mt-4'>
         <h1 className='text-white text-4xl block text-center font-extrabold my-12'>FAQ'S</h1>
-        <Faqs q={t('faq1')} a={<p>NFT stands for non-fungible-token. <a className='none' href='https://www.forbes.com/advisor/investing/cryptocurrency/nft-non-fungible-token/'>An NFT is a digital asset that represents real-world objects like art, music, in-game items, and videos. They are bought and sold online, frequently with cryptocurrency, and they are generally encoded with the same underlying software as many cryptos.</a> NFTs can be used to represent ownership of unique items; they allow tokenizing things like art, collectibles, and even real estate. They can only have one official owner at a time and they're secured by the Ethereum blockchain – no one can modify the record of ownership or copy/paste a new NFT into existence.</p>}/>
+        <Faqs q={t('faq1')} a={t("faq1Description")}/>
         <Faqs q={t("faq2")} a={t("faq2Description")}/>
         <Faqs q={t("faq3")} a={t('faq3Description')}/>
         <Faqs q={t("faq4")} a={t('faq4Description')}/>
